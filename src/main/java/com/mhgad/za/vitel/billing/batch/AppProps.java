0359d4f427cdf5c2985296f141b23aae6484407b
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-18 18:18:01 +0200

    # Added partner billing properties

--- src/main/java/com/mhgad/za/vitel/billing/batch/AppProps.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/AppProps.java
@@ -15,7 +15,23 @@ public class AppProps {
     private Integer chunkSize;
     @Value("${paging.size}")
     private Integer pagingSize;
+    
+    @Value("${partner.billing.jdbc.url}")
+    private String partnerBillingUrl;
+    @Value("${partner.billing.jdbc.datasource}")
+    private String partnerBillingDatasource;
+    @Value("${partner.billing.jdbc.username}")
+    private String partnerBillingUsername;
+    @Value("${partner.billing.jdbc.password}")
+    private String partnerBillingPassword;
 
+    @Value("${jdbc.cache.prepared.statements}")
+    private Boolean cachePrepStatements;
+    @Value("${jdbc.prepared.statement.cache.size}")
+    private Integer prepStatementCacheSize;
+    @Value("${jdbc.prepared.statement.cache.sql.limit}")
+    private Integer prepStatementCacheSqlLimit;
+    
     public AppProps() {
     }
 
@@ -30,4 +46,34 @@ public class AppProps {
     public Integer getPagingSize() {
         return pagingSize;
     }
+
+    public Boolean getCachePrepStatements() {
+        return cachePrepStatements;
+    }
+
+    public Integer getPrepStatementCacheSize() {
+        return prepStatementCacheSize;
+    }
+
+    public Integer getPrepStatementCacheSqlLimit() {
+        return prepStatementCacheSqlLimit;
+    }
+
+    public String getPartnerBillingUrl() {
+        return partnerBillingUrl;
+    }
+
+    public String getPartnerBillingDatasource() {
+        return partnerBillingDatasource;
+    }
+
+    public String getPartnerBillingUsername() {
+        return partnerBillingUsername;
+    }
+
+    public String getPartnerBillingPassword() {
+        return partnerBillingPassword;
+    }
+    
+    
 }
