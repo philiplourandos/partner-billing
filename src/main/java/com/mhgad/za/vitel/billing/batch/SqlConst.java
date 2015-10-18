67f2f2cfdf24d74e7455b01a9c59920857515604
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-18 13:01:00 +0200

    Constant file for SQL

---
+++ src/main/java/com/mhgad/za/vitel/billing/batch/SqlConst.java
@@ -0,0 +1,35 @@
+package com.mhgad.za.vitel.billing.batch;
+
+/**
+ *
+ * @author plourand
+ */
+public class SqlConst {
+    public static final String RETRIEVE_CDR_RECORDS_SELECT = 
+            "   SELECT "
+            + "     uniqueid,"
+            + "     calldate,"
+            + "     clid,"
+            + "     src,"
+            + "     dst,"
+            + "     dcontext,"
+            + "     channel,"
+            + "     dstchannel,"
+            + "     lastapp,"
+            + "     lastdata,"
+            + "     duration,"
+            + "     billsec,"
+            + "     disposition,"
+            + "     amaflags,"
+            + "     accountcode,"
+            + "     userfield,"
+            + "     cost";
+    public static final String RETRIEVE_CDR_RECORDS_FROM = "FROM cdr";
+    public static final String RETRIEVE_CDR_RECORDS_WHERE = 
+            "   WHERE "
+            + "     calldate BETWEEN :start AND :end "
+            + "     AND "
+            + "     (billsec > 0 AND disposition = 'ANSWERED' "
+            + "     AND "
+            + "     billsec < 10000 )";
+}
