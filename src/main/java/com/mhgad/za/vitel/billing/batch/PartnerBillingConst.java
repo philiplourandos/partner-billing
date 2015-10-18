7ce2a57bf95413555c18a827a9051154defa6e27
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-18 19:45:24 +0200

    # Constant file

---
+++ src/main/java/com/mhgad/za/vitel/billing/batch/PartnerBillingConst.java
@@ -0,0 +1,14 @@
+package com.mhgad.za.vitel.billing.batch;
+
+/**
+ *
+ * @author plourand
+ */
+public final class PartnerBillingConst {
+    
+    public static final String STATUS_CONTINUE = "continue";
+    
+    private PartnerBillingConst() {
+        
+    }
+}
