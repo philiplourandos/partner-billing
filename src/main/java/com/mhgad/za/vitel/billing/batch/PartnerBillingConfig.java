98c959e17dd518172648eda3aa2c33b066b625cd
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-18 13:02:05 +0200

    Added bean configuration for CDR reader

--- src/main/java/com/mhgad/za/vitel/billing/batch/PartnerBillingConfig.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/PartnerBillingConfig.java
@@ -1,8 +1,10 @@
 package com.mhgad.za.vitel.billing.batch;
 
-import com.mhgad.za.vitel.billing.batch.repo.DbServersRepo;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +16,23 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = {"com.mhgad.za.vitel.billing.batch"})
 @EnableBatchProcessing
 public class PartnerBillingConfig {
+    
+    @Autowired
+    private AppProps appProps;
+    
+    @Bean
+    public JdbcPagingItemReader cdrReader() throws Exception {
+        SqlPagingQueryProviderFactoryBean queryProvider = new SqlPagingQueryProviderFactoryBean();
+        queryProvider.setSelectClause(SqlConst.RETRIEVE_CDR_RECORDS_SELECT);
+        queryProvider.setFromClause(SqlConst.RETRIEVE_CDR_RECORDS_FROM);
+        queryProvider.setWhereClause(SqlConst.RETRIEVE_CDR_RECORDS_WHERE);
+
+        JdbcPagingItemReader reader = new JdbcPagingItemReader();
+        reader.setFetchSize(appProps.getFetchSize());
+        reader.setQueryProvider(queryProvider.getObject());
+        reader.setPageSize(appProps.getPagingSize());
+
+        return reader;
+    }
     
 }
