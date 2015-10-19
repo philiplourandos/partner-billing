72759881d6b6ee4eabd23694e863e6e14a228d78
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-19 05:41:58 +0200

    # Renamed file and moved spring batch tables to another file

---
+++ src/main/resources/db/migration/V1__setup_datasources.sql
@@ -0,0 +1,7 @@
+-- Partner billing tables
+CREATE TABLE db_servers(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    jdbc_url varchar(200) NOT NULL,
+    jdbc_username varchar(50) NOT NULL,
+    jdbc_password varchar(50) NOT NULL
+) ENGINE=InnoDB;
