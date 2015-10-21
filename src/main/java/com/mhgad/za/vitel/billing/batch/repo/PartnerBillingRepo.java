c251a3f25af7fbcb2f35c7549547c106657c2e18
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-21 18:02:33 +0200

    # Added method 'findAllSites'

--- src/main/java/com/mhgad/za/vitel/billing/batch/repo/PartnerBillingRepo.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/repo/PartnerBillingRepo.java
@@ -1,7 +1,9 @@
 package com.mhgad.za.vitel.billing.batch.repo;
 
 import com.mhgad.za.vitel.billing.batch.mapper.DbServersMapper;
+import com.mhgad.za.vitel.billing.batch.mapper.SiteMapper;
 import com.mhgad.za.vitel.billing.batch.model.DbServer;
+import com.mhgad.za.vitel.billing.batch.model.Site;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
@@ -13,7 +15,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class PartnerBillingRepo {
-    private static final String FIND_ALL_SERVER =
+    private static final String FIND_ALL_SERVERS =
             "   SELECT "
             + "     dbs.id,"
             + "     dbs.jdbc_url,"
@@ -21,6 +23,15 @@ public class PartnerBillingRepo {
             + "     dbs.jdbc_password"
             + " FROM "
             + "     DB_SERVERS dbs";
+    
+    private static final String FIND_ALL_SITES = 
+            "   SELECT "
+            + "     s.id, "
+            + "     s.name, "
+            + "     s.description, "
+            + "     s.outputFile "
+            + " FROM "
+            + "     site s";
 
     @Autowired
     private JdbcOperations ops;
@@ -29,6 +40,10 @@ public class PartnerBillingRepo {
     }
 
     public List<DbServer> findAllServers() {
-        return ops.query(FIND_ALL_SERVER, new DbServersMapper());
+        return ops.query(FIND_ALL_SERVERS, new DbServersMapper());
+    }
+
+    public List<Site> findAllSites() {
+        return ops.query(FIND_ALL_SITES, new SiteMapper());
     }
 }
