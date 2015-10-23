4acc18a2fb48762933eb40a2db341a27ff3c8070
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-23 07:34:02 +0200

    # destChannel was being supplied as query # corrected query as it was missing 'FROM $TABLE'

--- src/test/java/com/mhgad/za/vitel/billing/batch/repo/TestRepo.java
+++ src/test/java/com/mhgad/za/vitel/billing/batch/repo/TestRepo.java
@@ -1,5 +1,9 @@
 package com.mhgad.za.vitel.billing.batch.repo;
 
+import com.mhgad.za.vitel.billing.batch.SqlConst;
+import com.mhgad.za.vitel.billing.batch.mapper.CdrMapper;
+import com.mhgad.za.vitel.billing.batch.model.Cdr;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.stereotype.Repository;
@@ -16,11 +20,20 @@ public class TestRepo {
             + "     COUNT(uniqueid) AS total"
             + " FROM "
             + "     cdr";
-    
+
+    private static final String FIND_CDR_BY_DST_CHANNEL = SqlConst.RETRIEVE_CDR_RECORDS_SELECT 
+            + " " + SqlConst.RETRIEVE_CDR_RECORDS_FROM
+            + " WHERE "
+            + "     dstchannel = ?";
+
     @Autowired
     private JdbcOperations ops;
 
     public Integer countCdrs() {
         return ops.queryForObject(COUNT_CDR_ENTRIES, Integer.class);
+    }
+        
+    public List<Cdr> findByDestChannel(String destChannel) {
+        return ops.query(FIND_CDR_BY_DST_CHANNEL, new Object[]{destChannel}, new CdrMapper());
     }
 }
