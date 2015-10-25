7cf77a2710113849fdf5d34e3c4b1d3949a05384
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-25 11:50:07 +0200

    # Added site id to create statement

--- src/main/resources/db/migration/V1__setup_datasources.sql
+++ src/main/resources/db/migration/V1__setup_datasources.sql
@@ -3,5 +3,6 @@ CREATE TABLE db_servers(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     jdbc_url varchar(200) NOT NULL,
     jdbc_username varchar(50) NOT NULL,
-    jdbc_password varchar(50) NOT NULL
+    jdbc_password varchar(50) NOT NULL,
+    site_id INT NOT NULL
 ) ENGINE=InnoDB;
