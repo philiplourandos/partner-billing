3960163c3481ae34ebb3901b786cd04bb9543c29
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-25 11:48:08 +0200

    # Retrieve site from the resultset and set on the bean

--- src/main/java/com/mhgad/za/vitel/billing/batch/mapper/DbServersMapper.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/mapper/DbServersMapper.java
@@ -14,6 +14,7 @@ public class DbServersMapper implements RowMapper<DbServer> {
     private static final int URL_INDEX = 2;
     private static final int USERNAME_INDEX = 3;
     private static final int PASSWORD_INDEX = 4;
+    private static final int SITE_INDEX = 5;
 
     public DbServersMapper() {
     }
@@ -25,6 +26,7 @@ public class DbServersMapper implements RowMapper<DbServer> {
         pojo.setUrl(rs.getString(URL_INDEX));
         pojo.setUsername(rs.getString(USERNAME_INDEX));
         pojo.setPassword(rs.getString(PASSWORD_INDEX));
+        pojo.setSiteId(rs.getInt(SITE_INDEX));
 
         return pojo;
     }
