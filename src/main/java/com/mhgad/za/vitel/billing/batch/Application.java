c2857aba19a819477fde31e6d555721eded1717f
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-26 08:15:43 +0200

    # Added launcher for job

--- src/main/java/com/mhgad/za/vitel/billing/batch/Application.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/Application.java
@@ -1,11 +1,22 @@
 package com.mhgad.za.vitel.billing.batch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  *
  * @author plourand
  */
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        AnnotationConfigApplicationContext context = 
+                new AnnotationConfigApplicationContext("com.mhgad.za.vitel.billing.batch");
         
+        final Job generateReports = context.getBean(Job.class);
+        final JobLauncher launcher = context.getBean(JobLauncher.class);
+        
+        launcher.run(generateReports, new JobParametersBuilder().toJobParameters());
     }
 }
