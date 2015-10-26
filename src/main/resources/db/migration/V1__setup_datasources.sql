5074fe3161c5dd5158465c42a1298b31fb97e851
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-26 14:26:33 +0200

    # Corrected table name to uppercase as mysql is case sensitive

--- src/main/resources/db/migration/V1__setup_datasources.sql
+++ src/main/resources/db/migration/V1__setup_datasources.sql
@@ -1,5 +1,5 @@
 -- Partner billing tables
-CREATE TABLE db_servers(
+CREATE TABLE DB_SERVERS(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     jdbc_url varchar(200) NOT NULL,
     jdbc_username varchar(50) NOT NULL,
