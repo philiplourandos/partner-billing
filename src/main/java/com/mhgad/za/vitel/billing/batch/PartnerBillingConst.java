5a493e173e0c7a4191e64900eb5946078960b346
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-27 08:02:48 +0200

    # Added constants for start and end date job parameters

--- src/main/java/com/mhgad/za/vitel/billing/batch/PartnerBillingConst.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/PartnerBillingConst.java
@@ -7,6 +7,8 @@ package com.mhgad.za.vitel.billing.batch;
 public final class PartnerBillingConst {
     
     public static final String STATUS_CONTINUE = "continue";
+    public static final String PARAM_START_DATE = "cdr.start.date";
+    public static final String PARAM_END_DATE = "cdr.end.date";
     
     private PartnerBillingConst() {
         
