89a9a886b9ac8bd9d87e9e4a1bfc986997fb01ef
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-27 08:02:28 +0200

    # Added check for arguments passed into app # Add args as parameters for job

--- src/main/java/com/mhgad/za/vitel/billing/batch/Application.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/Application.java
@@ -1,6 +1,5 @@
 package com.mhgad.za.vitel.billing.batch;
 
-import java.util.Date;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.batch.core.Job;
@@ -16,19 +15,27 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
     private static final Logger LOG = LogManager.getLogger(Application.class);
 
+    private static final int EXPECTED_ARG_COUNT = 2;
+    
     public static void main(String[] args) throws Exception {
-        AnnotationConfigApplicationContext context = 
-                new AnnotationConfigApplicationContext("com.mhgad.za.vitel.billing.batch");
         
-        final Job generateReports = context.getBean(Job.class);
-        final JobLauncher launcher = context.getBean(JobLauncher.class);
+        if (args.length != EXPECTED_ARG_COUNT) {
+            LOG.error("cdr.start.date and cdr.end.date values must be passed in");
+        } else {
+            AnnotationConfigApplicationContext context = 
+                    new AnnotationConfigApplicationContext("com.mhgad.za.vitel.billing.batch");
 
-        JobParametersBuilder paramBuilder = new JobParametersBuilder();
-        paramBuilder.addDate("run.date", new Date());
+            final Job generateReports = context.getBean(Job.class);
+            final JobLauncher launcher = context.getBean(JobLauncher.class);
 
-        LOG.info("Starting partner billing run.");
-        JobExecution runStatus = launcher.run(generateReports, paramBuilder.toJobParameters());
+            JobParametersBuilder paramBuilder = new JobParametersBuilder();
+            paramBuilder.addString(PartnerBillingConst.PARAM_START_DATE, args[0]);
+            paramBuilder.addString(PartnerBillingConst.PARAM_END_DATE, args[1]);
 
-        LOG.info("Run completed, status: %s", runStatus.getExitStatus());
+            LOG.info("Starting partner billing run.");
+            JobExecution runStatus = launcher.run(generateReports, paramBuilder.toJobParameters());
+
+            LOG.info("Run completed, status: %s", runStatus.getExitStatus());
+        }
     }
 }
