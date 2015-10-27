43c568722022fc467cbd8daa8fc952c81bdfcb48
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-27 08:00:34 +0200

    # Pass start and end dates into application

--- src/assembly/bin/run.sh
+++ src/assembly/bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.Application
+java -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.Application $1 $2
