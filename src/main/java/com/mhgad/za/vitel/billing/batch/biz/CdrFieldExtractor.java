a6faa3e46fa7bfa4d708aa04991977f75bb80db6
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-27 12:36:29 +0200

    # Corrected quoting around fields

--- src/main/java/com/mhgad/za/vitel/billing/batch/biz/CdrFieldExtractor.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/biz/CdrFieldExtractor.java
@@ -13,6 +13,8 @@ public class CdrFieldExtractor implements FieldExtractor<Cdr> {
     private static final int FIELD_COUNT = 17;
     private static final String DOUBLE_QUOTE = "\"";
 
+    private static final Double ZERO = new Double(0.0);
+    
     public CdrFieldExtractor() {
     }
 
@@ -30,16 +32,16 @@ public class CdrFieldExtractor implements FieldExtractor<Cdr> {
         values[6] = DOUBLE_QUOTE + item.getDstchannel() + DOUBLE_QUOTE;
         values[7] = DOUBLE_QUOTE + item.getLastapp() + DOUBLE_QUOTE;
         values[8] = DOUBLE_QUOTE + item.getLastdata() + DOUBLE_QUOTE;
-        values[9] = DOUBLE_QUOTE + item.getDuration() + DOUBLE_QUOTE;
-        values[10] = DOUBLE_QUOTE + item.getBillsec() + DOUBLE_QUOTE;
+        values[9] = item.getDuration();
+        values[10] = item.getBillsec();
         values[11] = DOUBLE_QUOTE + item.getDisposition() + DOUBLE_QUOTE;
-        values[12] = DOUBLE_QUOTE + item.getAmaflags() + DOUBLE_QUOTE;
+        values[12] = item.getAmaflags();
         values[13] = DOUBLE_QUOTE + item.getAccountcode() + DOUBLE_QUOTE;
         values[14] = DOUBLE_QUOTE + item.getUniqueid() + DOUBLE_QUOTE;
         values[15] = DOUBLE_QUOTE + item.getUserfield() + DOUBLE_QUOTE;
 
         Double cost = item.getCost();
-        if (cost != null) {
+        if (cost != null && !ZERO.equals(cost)) {
             values[16] = DOUBLE_QUOTE + cost + DOUBLE_QUOTE;
         } else {
             values[16] = DOUBLE_QUOTE + DOUBLE_QUOTE;
