6361f827492e56115a8d5d2968c6d203ac2eb4e0
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-28 10:48:31 +0200

    # Corrected package where Application is in

--- src/assembly/bin/run.sh
+++ src/assembly/bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.Application $1 $2
+java -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.extract.Application $1 $2
