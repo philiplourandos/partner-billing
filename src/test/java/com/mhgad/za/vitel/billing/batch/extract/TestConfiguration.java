dcdf4590b3b240c70bcdeb10e3b65e5e16d266be
Philip Lourandos <philip.lourandos@gmail.com>
2015-10-29 13:10:50 +0200

    # Corrected packages so that they are component scanned

---
+++ src/test/java/com/mhgad/za/vitel/billing/batch/extract/TestConfiguration.java
@@ -0,0 +1,27 @@
+package com.mhgad.za.vitel.billing.batch.extract;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+/**
+ *
+ * @author plourand
+ */
+@Configuration
+public class TestConfiguration {
+    @Bean
+    public DataSource initDb() {
+        EmbeddedDatabase ds = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .addScript("test-db.sql")
+                .setName("test")
+                .build();
+
+        return ds;
+    }
+}
---
+++ src/test/java/com/mhgad/za/vitel/billing/batch/extract/TestConst.java
@@ -0,0 +1,14 @@
+package com.mhgad.za.vitel.billing.batch.extract;
+
+/**
+ *
+ * @author plourand
+ */
+public class TestConst {
+
+    public static final String START = "2000-01-01";
+    public static final String END = "2016-01-01";
+
+    private TestConst() {
+    }
+}
