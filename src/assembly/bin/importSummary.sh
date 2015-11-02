8f1af15ff8d764e0250c63c573b84e54b6a4fec4
Philip Lourandos <philip.lourandos@gmail.com>
2015-11-02 13:47:02 +0200

    # Added switches for arguments supplied to the batch applications

--- src/assembly/bin/importSummary.sh
+++ src/assembly/bin/importSummary.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.aspivia.Application $1 $2 $3
+java -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.aspivia.Application -i $1 -s $2 -o $3
 
--- src/assembly/bin/importSummaryDebug.sh
+++ src/assembly/bin/importSummaryDebug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.aspivia.Application $1 $2 $3
+java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=8000 -cp "../lib/*:../etc" -Dspring.profiles.active=prod com.mhgad.za.vitel.billing.batch.aspivia.Application -i $1 -s $2 -o $3
