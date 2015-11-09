26f1e1231103af9ad9123925520ebf280a6f0d5e
Philip Lourandos <philip.lourandos@gmail.com>
2015-11-09 13:28:58 +0200

    # Added prelim total

--- src/main/java/com/mhgad/za/vitel/billing/batch/aspivia/AspiviaConst.java
+++ src/main/java/com/mhgad/za/vitel/billing/batch/aspivia/AspiviaConst.java
@@ -7,7 +7,7 @@ package com.mhgad.za.vitel.billing.batch.aspivia;
 public final class AspiviaConst {
 
     public static final String SUMMARY_KEY = "aspivia.summaries";
-    public static final String SUMMARY_HEADER = "Code,Partner,Calls,Rand Out,Rand In,Total";
+    public static final String SUMMARY_HEADER = "Code,Partner,Calls,Rand Out,Rand In,Prelim Total,Total";
     public static final String SITE_ID = "site.id";
     
     public static final String PARAM_INPUT_FILE = "input.file";
