edd0242b519357551b337588101d6d49e5c5f82e
Philip Lourandos <philip.lourandos@gmail.com>
2020-06-28 18:19:30 +0200

    * Removed bean method `partnerBillingDs` as spring boot will create a datasource * Removed transaction manager bean as the datasource should have a txn manager * Made parameters and local vars `final`

--- partner-billing-extract/src/main/java/com/mhgad/za/vitel/billing/batch/extract/PartnerBillingConfig.java
+++ partner-billing-extract/src/main/java/com/mhgad/za/vitel/billing/batch/extract/PartnerBillingConfig.java
@@ -1,7 +1,6 @@
 package com.mhgad.za.vitel.billing.batch.extract;
 
 import com.mhgad.za.vitel.billing.batch.common.SqlConst;
-import com.mhgad.za.vitel.billing.batch.common.AppProps;
 import com.mhgad.za.vitel.billing.batch.extract.biz.CdrFieldExtractor;
 import com.mhgad.za.vitel.billing.batch.extract.biz.CdrProcessor;
 import com.mhgad.za.vitel.billing.batch.extract.decision.NextDatasourceDecision;
@@ -10,8 +9,6 @@ import com.mhgad.za.vitel.billing.batch.extract.mapper.CdrMapper;
 import com.mhgad.za.vitel.billing.batch.extract.model.Cdr;
 import com.mhgad.za.vitel.billing.batch.extract.tasklet.DatasourceSupplierTasklet;
 import com.mhgad.za.vitel.billing.batch.extract.tasklet.SiteSupplierTasklet;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -19,7 +16,6 @@ import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
@@ -36,27 +32,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
-/**
- *
- * @author plourand
- */
 @ComponentScan(basePackages = {"com.mhgad.za.vitel.billing.batch.extract",
     "com.mhgad.za.vitel.billing.batch.common"})
 @Configuration
-@EnableBatchProcessing
-@EnableAspectJAutoProxy
-@PropertySource("classpath:/app.properties")
 public class PartnerBillingConfig {
 
     @Autowired
-    private AppProps appProps;
+    private ExtractProps appProps;
 
     @Autowired
     private DatasourceSupplierTasklet dsSupplier;
@@ -66,28 +50,12 @@ public class PartnerBillingConfig {
 
     @Autowired
     private NextDatasourceDecision dsDecision;
-    
+
     @Autowired
     private NextSiteDecision siteDecision;
-    
+
     @Autowired
     private CdrProcessor costItemProc;
-
-    @Bean
-    @Profile("prod")
-    public DataSource partnerBillingDs() {
-        final HikariConfig cfg = new HikariConfig();
-        cfg.setJdbcUrl(appProps.getPartnerBillingUrl());
-        cfg.setUsername(appProps.getPartnerBillingUsername());
-        cfg.setPassword(appProps.getPartnerBillingPassword());
-        cfg.addDataSourceProperty("cachePrepStmts", appProps.getCachePrepStatements());
-        cfg.addDataSourceProperty("prepStmtCacheSize", appProps.getPrepStatementCacheSize());
-        cfg.addDataSourceProperty("prepStmtCacheSqlLimit", appProps.getPrepStatementCacheSqlLimit());
-
-        final HikariDataSource ds = new HikariDataSource(cfg);
-
-        return ds;
-    }
 
     @Bean
     public JdbcTemplate partnerBillingTemplate(final DataSource partnerBillingDs) {
@@ -138,13 +106,13 @@ public class PartnerBillingConfig {
 
         return reader;
     }
-    
+
     @Bean
     public FlatFileItemWriter<Cdr> fileoutWriter() {
         final DelimitedLineAggregator lineAgg = new DelimitedLineAggregator();
         lineAgg.setDelimiter(",");
         lineAgg.setFieldExtractor(new CdrFieldExtractor());
-        
+
         final FlatFileItemWriter<Cdr> writer = new FlatFileItemWriter<>();
         writer.setEncoding("UTF-8");
         writer.setLineAggregator(lineAgg);
@@ -155,10 +123,9 @@ public class PartnerBillingConfig {
     @Bean
     public Job createJob(final StepBuilderFactory stepBuilderFactory, final JobBuilderFactory jobs,
             final DataSource partnerBillingDs) throws Exception {
-        Step getDatasource = stepBuilderFactory.get("getDatasource").tasklet(dsSupplier).build();
-        
+        final Step getDatasource = stepBuilderFactory.get("getDatasource").tasklet(dsSupplier).build();
         final JdbcPagingItemReader reader = cdrReader(partnerBillingDs);
-        
+
         final Step retrieveCdrs = stepBuilderFactory.get("stepRetrieveCdrs")
                 .<Cdr, Cdr>chunk(appProps.getChunkSize())
                 .reader(reader)
@@ -187,23 +154,13 @@ public class PartnerBillingConfig {
                 .from(siteDecision).on(FlowExecutionStatus.COMPLETED.toString())
                 .to(endStep(stepBuilderFactory)).end().build();
     }
-    
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer propPlaceholder() {
-        return new PropertySourcesPlaceholderConfigurer();
-    }
 
-    @Bean
-    public DataSourceTransactionManager transactionManager(final DataSource partnerBillingDs) {
-        return new DataSourceTransactionManager(partnerBillingDs);
-    }
-    
-    public Step endStep(StepBuilderFactory stepBuilderFactory) {
+    public Step endStep(final StepBuilderFactory stepBuilderFactory) {
         final Tasklet task = (contribution, chunkContext) -> {return RepeatStatus.FINISHED;};
-        
+
         return stepBuilderFactory.get("end").tasklet(task).build();
     }
-    
+
     /**
      * Helper step to get the start and end date from the job context as the values
      * are now passed in via the job launcher.
@@ -213,23 +170,23 @@ public class PartnerBillingConfig {
      * 
      * @return Step returned
      */
-    public Step paramsStep(StepBuilderFactory stepBuilderFactory, JdbcPagingItemReader reader) {
+    public Step paramsStep(final StepBuilderFactory stepBuilderFactory, final JdbcPagingItemReader reader) {
         Tasklet task = (contribution, chunkContext) -> {
             final Map<String, Object> jobsParams = chunkContext.getStepContext().getJobParameters();
-            appProps.setStartDate((String) jobsParams.get(PartnerBillingConst.PARAM_START_DATE));
-            appProps.setEndDate((String) jobsParams.get(PartnerBillingConst.PARAM_END_DATE));
-            
+            final String startDate = (String) jobsParams.get(PartnerBillingConst.PARAM_START_DATE);
+            final String endDate = (String) jobsParams.get(PartnerBillingConst.PARAM_END_DATE);
+
             final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
 
             final Map<String, Date> params = new HashMap<>();
-            params.put("start", formatter.parse(appProps.getStartDate()));
-            params.put("end", formatter.parse(appProps.getEndDate()));
+            params.put("start", formatter.parse(startDate));
+            params.put("end", formatter.parse(endDate));
 
             reader.setParameterValues(params);
-            
+
             return RepeatStatus.FINISHED;
         };
-        
+
         return stepBuilderFactory.get("params").tasklet(task).build();
     }
 }
